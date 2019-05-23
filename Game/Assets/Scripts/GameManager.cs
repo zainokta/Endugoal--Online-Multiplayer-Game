20b04b689d680bb39edc1705371872e5d0d5eecf
zainokta <zainokta@gmail.com>
2019-05-23 10:02:17 +0700

    Add Core Gameplay

---
+++ Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Sepay;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] arrayOfPlayers;
+    private int hostScore, awayScore;
+    public bool isHost = true;
+    [SerializeField] GameObject ball;
+    Button btn;
+
+    public int HostScore { get => hostScore; set => hostScore = value; }
+    public int AwayScore { get => awayScore; set => awayScore = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitPlayers();
+    }
+
+    private void InitPlayers()
+    {
+        arrayOfPlayers[0].GetComponent<Player>().IsHostPlayer = true;
+        arrayOfPlayers[0].GetComponent<Player>().IsHost = isHost;
+        arrayOfPlayers[1].GetComponent<Player>().IsHostPlayer = false;
+        arrayOfPlayers[1].GetComponent<Player>().IsHost = isHost;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            MovePlayer(Vector2.left * 4);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            MovePlayer(Vector2.right * 4);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        if(isHost)
+            arrayOfPlayers[0].GetComponent<Rigidbody2D>().velocity = new Vector2(arrayOfPlayers[0].GetComponent<Rigidbody2D>().velocity.x, 10);
+        else
+            arrayOfPlayers[1].GetComponent<Rigidbody2D>().velocity = new Vector2(arrayOfPlayers[1].GetComponent<Rigidbody2D>().velocity.x, 10);
+    }
+
+    private void MovePlayer(Vector2 _velocity)
+    {
+        if (isHost)
+            arrayOfPlayers[0].GetComponent<Rigidbody2D>().velocity = new Vector2(_velocity.x, arrayOfPlayers[0].GetComponent<Rigidbody2D>().velocity.y);
+        
+        else
+            arrayOfPlayers[1].GetComponent<Rigidbody2D>().velocity = new Vector2(_velocity.x, arrayOfPlayers[1].GetComponent<Rigidbody2D>().velocity.y);
+        
+    }
+
+    public void Goal(bool _homeGoal)
+    {
+        ball.transform.position = new Vector3(0, 3);
+        ball.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        ball.GetComponent<Rigidbody2D>().angularVelocity = 0;
+
+        arrayOfPlayers[0].transform.position = new Vector3(-5, -1.3f);
+        arrayOfPlayers[1].transform.position = new Vector3(5, -1.3f);
+
+        if (_homeGoal)
+            hostScore += 1;
+        else
+            awayScore += 1;
+    }
+}
---
+++ Game/Assets/Scripts/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sepay
+{
+    public class Player : MonoBehaviour
+    {
+        private bool isHost;
+        private bool isHostPlayer;
+
+        public bool IsHost { get => isHost; set => isHost = value; }
+        public bool IsHostPlayer { get => isHostPlayer; set => isHostPlayer = value; }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnCollisionStay2D(Collision2D collision)
+        {
+            //Debug.Log(collision.gameObject.tag);
+            if (collision.gameObject.tag == "Ball")
+            {
+                if (Input.GetKey(KeyCode.Z))
+                {
+                    Debug.Log("Z Press and collide");
+                    if (IsHost && IsHostPlayer && collision.gameObject.transform.position.x > transform.position.x)
+                    {
+                        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(1, 0.1f) * 500);
+                        Debug.Log("Sepak");
+                    }
+
+                    if (!IsHost && !IsHostPlayer && collision.gameObject.transform.position.x < transform.position.x)
+                    {
+                        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-1, 0.1f) * 500);
+                    }
+                }
+
+                if (Input.GetKey(KeyCode.X))
+                {
+                    Debug.Log("X Press and collide");
+                    if (IsHost && IsHostPlayer && collision.gameObject.transform.position.x > transform.position.x)
+                    {
+                        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0.5f, 1) * 500);
+                        Debug.Log("Sepak munggah");
+                    }
+
+                    if (!IsHost && !IsHostPlayer && collision.gameObject.transform.position.x < transform.position.x)
+                    {
+                        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-0.5f, 1) * 500);
+                    }
+                }
+            }
+        }
+    }
+
+}
