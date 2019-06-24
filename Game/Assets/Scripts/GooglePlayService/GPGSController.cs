a57b2bb69ceca4be403f4ca271d58c62c5ce75f2
Reyhan Alphard Savero <newrey9227@gmail.com>
2019-06-24 13:59:27 +0700

    Add | Google Play Service

---
+++ Game/Assets/Scripts/GooglePlayService/GPGSController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GooglePlayGames.BasicApi;
+using GooglePlayGames;
+
+public class GPGSController : MonoBehaviour
+{
+    public GameObject loginBtn, achievementBtn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Login();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Login()
+    {
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder().Build();
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+
+        Social.localUser.Authenticate((bool success) =>
+        {
+            if (success == true)
+            {
+                loginBtn.SetActive(false);
+                achievementBtn.SetActive(true);
+                FirstLogin();
+            }
+            else
+            {
+                achievementBtn.SetActive(false);
+            }
+        });
+    }
+
+    void FirstLogin()
+    {
+        PlayGamesPlatform.Instance.ReportProgress(GPGSIds.achievement_first_login, 100f, null);
+    }
+
+    public void ShowAchievement()
+    {
+        PlayGamesPlatform.Instance.ShowAchievementsUI();
+    }
+}
